3d213bc22c24b7798d0a0b8de53931e49055a9a2
meganmilesoneill <meganmilesoneill@gmail.com>
2015-11-21 23:10:25 -0800

    adding initial files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # datascience_tidydata
+
+## Summary
+This repository contains code that transforms data from the "Human Activity Recognition Using Smartphones Data Set" available on the UCI Machine Learning Repository site here: <http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones>.
+
+The goal of the script is to combine train and test data into a single dataset, and then use the mean and standard deviation features to generate summary data for each user and activity type.
+
+## Steps to reproduce the data
+The following steps were created on a Mac/OSX Yosemite (10.10.5) using RStudio Version 0.99.485. 
+
+* Execute the code in "run_analysis.R" in order to reproduce the data.
+* The output of this script is a comma-delimited text file including headers.  The file is called "tiny_data.txt"
+* The script unzips the "UCI HAR Dataset.zip" file which contains the files used by "run_analysis.R".  The files used by the script are outlined in the CodeBook.md file.
---
+++ run_analysis.R
@@ -0,0 +1,65 @@
+unzip("UCI HAR Dataset.zip")
+
+#load train data
+train <- read.table("./UCI HAR Dataset/train/X_train.txt", header=FALSE)
+train_y <- read.table("./UCI HAR Dataset/train/y_train.txt", header=FALSE, col.names = "activity.code")
+train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt", header=FALSE, col.names = "subject")
+#add subject data to feature data
+train$subject <- train_subject$subject
+#add activity.code to feature data
+train$activity.code <- train_y$activity.code
+
+#load test data
+test <- read.table("./UCI HAR Dataset/test/X_test.txt", header=FALSE)
+test_y <- read.table("./UCI HAR Dataset/test/y_test.txt", header=FALSE, col.names = "activity.code")
+test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt", header=FALSE, col.names = "subject")
+#add subject data to feature data
+test$subject <- test_subject$subject
+#add activity.code to feature data
+test$activity.code <- test_y$activity.code
+
+#combine train and test data into a single data.frame
+all.data <- rbind(train, test)
+
+#load features
+features <- read.table("./UCI HAR Dataset/features.txt", header=FALSE)
+#clean up label names
+features$friendly.name <- gsub("-", ".", features$V2)
+features$friendly.name <- gsub(",", ".", features$V2)
+features$friendly.name <- gsub("\\(", "", features$friendly.name)
+features$friendly.name <- gsub("\\)", "", features$friendly.name)
+
+#since we will be using this master list of features to name the columns in the "all.data" data.frame,
+#we include here the two additional columns added when combining the label and subject data 
+#with the feature data
+features <- rbind(features, 
+                data.frame(V1=c(562, 563), V2=c("activity.code", "subject"), 
+                friendly.name=c("activity.code", "subject")))
+
+#subset the features such that they contain only mean and std features
+#in addition to the activity.code and subject features
+features.mean.std <- subset(features,  
+                          grepl(glob2rx("*mean()-*"), V2) | 
+                              grepl(glob2rx("*std()-*"), V2) | 
+                              (V2 %in% c("activity.code", "subject")))
+
+#use the features to assign column names
+colnames(all.data) <- features$friendly.name
+
+#subset the data to contain only the mean and std columns
+all.data.mean.std <- all.data[,features.mean.std$V1]
+
+#assign descriptive activity features to each row
+activity.labels.data <- read.table("./UCI HAR Dataset/activity_labels.txt", header=FALSE, col.names = c("activity.code", "activity.name"))
+all.data.final <- merge(all.data.mean.std, activity.labels.data, by="activity.code")[, union(names(all.data.mean.std), names(activity.labels.data))]
+
+head(all.data.final)
+
+#aggregate the data by subject and activity.name using the mean function
+all.data.summary <- aggregate(. ~ subject+activity.name, data=all.data.final, mean)
+
+head(all.data.summary)
+
+write.table(all.data.summary, "tidy_data.txt", sep=",", row.names = FALSE)
+
+features
